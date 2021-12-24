fea19eeeb4a2e9c456f47f778cc03b2a3d0bb8bf
N180885 <teskarisiparol@gmail.com>
2021-12-24 23:08:24 +0300

    8

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Router} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
 import Nav from "./components/nav";
 import Main from "./components/main";
